0b52503aeceb7c13870e60f91dd41de9471678ff
Rashed <migdadyrashed@gmail.com>
2022-03-21 12:17:44 +0300

    add new folder with name serviceMethods to hold all APIs

--- src/Helper/HTTPMethod.Helper.jsx
+++ src/Helper/HTTPMethod.Helper.jsx
@@ -1,14 +1,14 @@
 const { default: axios } = require("axios");
- 
+
 export const HTTPServices = {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete:axios.delete,
+    delete: axios.delete,
 }
 
 const token = localStorage.getItem("token");
- export const serverAddress = "http://localhost:5000";
+export const serverAddress = "http://localhost:5000";
 
-export const Auth = {Headers: {Authorization: `Bearer ${token}`}};
+export const Auth = { Headers: { Authorization: `Bearer ${token}` } };
 
